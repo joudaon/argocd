45a0ffd2996ac6c8c94a51c229b92582c99a7fb1
joudaon <jonuda77@gmail.com>
2023-12-16 15:10:23 +0100

    argocd installation updated

--- real_case/install/install.sh
+++ real_case/install/install.sh
@@ -9,24 +9,19 @@ minikube start --addons=dashboard --addons=metrics-server --addons=ingress --add
 
 ## Install argocd
 echo "---> Installing argocd"
-# kubectl create namespace argocd
-# kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 helm repo add argo https://argoproj.github.io/argo-helm
 helm repo update
 helm install argo argo/argo-cd -f argocd_values.yaml --namespace argocd --version 5.51.6 --create-namespace --wait 
-kubectl expose deployment argo-argocd-server --type=NodePort --name=argo-argocd-service --namespace=argocd
 sleep 30s
 
-# ## Download argocd cli (uncomment if argocd cli not installed)
-# VERSION="v2.6.6" # Select desired TAG from https://github.com/argoproj/argo-cd/releases
-# curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/download/$VERSION/argocd-linux-amd64
-# sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
-# rm argocd-linux-amd64
+## Bootstrap argo apps
+echo "--> Bootstrapping argo applications"
+kubectl apply -f ../argocd/bootstrap.yaml
 
 ## Get credentials
 echo "---> Installation finished!"
 CREDFILENAME=credentials.txt
-NODEPORT=$(kubectl get service argo-argocd-service --namespace=argocd -ojsonpath='{.spec.ports[0].nodePort}')
+NODEPORT=$(kubectl get service argo-argocd-server --namespace=argocd -ojsonpath='{.spec.ports[0].nodePort}')
 ARGOCD_PASSWORD=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
 echo "argocd URL --> $(minikube ip -p argocd-cluster):$NODEPORT" >> $CREDFILENAME
 echo "argocd username --> admin" >> $CREDFILENAME
