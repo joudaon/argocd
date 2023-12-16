0376c237510f367c36d3d321a7af774ef8a58d3b
joudaon <jonuda77@gmail.com>
2023-12-16 14:14:23 +0100

    argocd managing itself support added

--- real_case/install/install.sh
+++ real_case/install/install.sh
@@ -9,26 +9,13 @@ minikube start --addons=dashboard --addons=metrics-server --addons=ingress --add
 
 ## Install argocd
 echo "---> Installing argocd"
-kubectl create namespace argocd
+# kubectl create namespace argocd
 # kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 helm repo add argo https://argoproj.github.io/argo-helm
-helm install argo-cd argo/argo-cd -f argocd_values.yaml --namespace argocd --create-namespace --wait 
-kubectl expose deployment argo-cd-argocd-server --type=NodePort --name=argo-cd-argocd-service --namespace=argocd
+helm repo update
+helm install argo argo/argo-cd -f argocd_values.yaml --namespace argocd --version 5.51.6 --create-namespace --wait 
+kubectl expose deployment argo-argocd-server --type=NodePort --name=argo-argocd-service --namespace=argocd
 sleep 30s
-
-## Install External Secrets Operator (https://github.com/external-secrets/external-secrets)(https://github.com/hashicorp/vault-helm/issues/17)
-# echo "---> Installing External Secrets Operator"
-# helm repo add external-secrets https://charts.external-secrets.io
-# helm install external-secrets external-secrets/external-secrets -n external-secrets --create-namespace --wait
-
-## Install Argo Rollouts
-echo "---> Installing Argo Rollouts"
-kubectl create namespace argo-rollouts
-kubectl apply -n argo-rollouts -f https://github.com/argoproj/argo-rollouts/releases/latest/download/install.yaml
-
-## Install Argo CD Image Updater (https://argocd-image-updater.readthedocs.io/en/stable/) Installed with argocd
-# echo "---> Installing Argo CD Image Updater"
-# kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj-labs/argocd-image-updater/stable/manifests/install.yaml
 
 # ## Download argocd cli (uncomment if argocd cli not installed)
 # VERSION="v2.6.6" # Select desired TAG from https://github.com/argoproj/argo-cd/releases
@@ -39,7 +26,7 @@ kubectl apply -n argo-rollouts -f https://github.com/argoproj/argo-rollouts/rele
 ## Get credentials
 echo "---> Installation finished!"
 CREDFILENAME=credentials.txt
-NODEPORT=$(kubectl get service argo-cd-argocd-service --namespace=argocd -ojsonpath='{.spec.ports[0].nodePort}')
+NODEPORT=$(kubectl get service argo-argocd-service --namespace=argocd -ojsonpath='{.spec.ports[0].nodePort}')
 ARGOCD_PASSWORD=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
 echo "argocd URL --> $(minikube ip -p argocd-cluster):$NODEPORT" >> $CREDFILENAME
 echo "argocd username --> admin" >> $CREDFILENAME
